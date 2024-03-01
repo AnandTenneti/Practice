872d5d928c6171884676a6a251ef58e66c0aca3c
AnnadTenneti <tenneti.anand@gmail.com>
2024-03-01 13:32:29 +0530

    Commit the change

---
+++ PracticeProject/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        int sum = a + b;
+        System.out.println("Sum of the two numbers is " + sum);
+    }
+}
