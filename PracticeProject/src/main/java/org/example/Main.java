dd2f43b7b87e606d3a83cb35e55ecaba09ba4269
AnnadTenneti <tenneti.anand@gmail.com>
2024-03-01 14:45:14 +0530

    Commit the change

--- PracticeProject/src/main/java/org/example/Main.java
+++ PracticeProject/src/main/java/org/example/Main.java
@@ -3,8 +3,12 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         int a = 5;
-        int b = 2;
+        int b = 4;
         int sum = a + b;
         System.out.println("Sum of the two numbers is " + sum);
+        int c = 7;
+        int d = 4;
+        int subtract = c - d;
+        System.out.println("Sum of the two numbers is " + subtract);
     }
 }
